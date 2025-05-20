df1fac13b6bc698ea4a18f6e4477f396c4eb1d7a
Tawatchai Jitporn <mr.tawatchai@live.com>
2025-05-20 09:50:53 +0700

    Add theme switcher

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,4 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 
 export const Header = () => {
     return (
@@ -8,6 +9,7 @@ export const Header = () => {
                 <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <h1>TJ the Calculator's Portfolio Website</h1>
+            <ModeToggle />
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Router from "@/router";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <BrowserRouter>
-            <Router />
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Router />
+            </ThemeProvider>
         </BrowserRouter>
     </StrictMode>
 );
